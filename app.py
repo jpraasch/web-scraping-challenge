3c8a84fbc5f45fae7d2b3e87ed3a452807481f2e
Joe Raasch <jpraasch@gmail.com>
2021-04-01 20:39:02 -0500

    Repo change due corruption and updated index and app

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
+import pymongo
 import scrape_mars
 
+# Use PyMongo to establish Mongo connection
+client = pymongo.MongoClient('mongodb://localhost:27017')
+db = client.mars_db
+collection = db.mars
+# Create an instance of Flask
 app = Flask(__name__)
 
-mongo = PyMongo(app)
 
-@app.route("/")
-def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars = mars)
 
-@app.route("/scrape")
+
+
+# Route to render index.html template using data from Mongo
+@app.route('/')
+def home():
+	mars = collection.find_one()
+	return render_template('index.html', mars=mars)
+
+@app.route('/scrape')
 def scrape():
-    mars = mongo.db.mars 
-    mars_data = scrape_mars.scrape()
-    mars.update({}, mars_data, upsert=True)
-    return redirect("http://localhost:5000/", code=302)
+	scrape_mars.scrape()
+	return redirect('/', code = 302)
+
+ 
+
 
 if __name__ == "__main__":
     app.run(debug=True)
